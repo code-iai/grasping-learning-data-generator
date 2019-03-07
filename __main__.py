68b03af387178351bb4ecf8a64bf119abc831ba3
Sebastian Koralewski <seba@cs.uni-bremen.de>
2019-03-07 10:10:25 +0100

    refactor(cram2wordnet.py): Extracts cram to WordNet mapping in a single module.

--- __main__.py
+++ __main__.py
@@ -2,13 +2,9 @@ import sys
 from os import listdir
 from os.path import isdir, join
 
-import pandas as pd
-
 from grasping_learning_data_generator.orientation import get_grasping_type_learning_data, \
     transform_grasping_type_data_point_into_mln_database
-from grasping_learning_data_generator.position import get_learning_data_from_neems
-
-_cram_to_word_net_object_ = {'BOWL':'bowl.n.01', 'CUP': 'cup.n.01', 'SPOON' : 'spoon.n.01'}
+from grasping_learning_data_generator.position import generate_learning_data_from_neems
 
 
 def transform_neem_to_mln_databases(neem_path, result_path):
@@ -29,23 +25,10 @@ if __name__ == "__main__":
     result_dir_path = args[1]
 
     if isdir(path):
-        csv_data_frame = get_learning_data_from_neems(path)
+        csv_data_frame = generate_learning_data_from_neems(path, result_dir_path)
 
         for experiment_file in listdir(path):
             experiment_path = join(path, experiment_file)
             transform_neem_to_mln_databases(experiment_path, result_dir_path)
-
-        object_type = csv_data_frame['object_type'].unique()[0]
-        object_type = _cram_to_word_net_object_.get(object_type,object_type)
-        
-        for grasping_type in csv_data_frame['grasp'].unique():
-            for arm in csv_data_frame['arm'].unique():
-                for faces in csv_data_frame['result'].unique():
-                    grasping_type_based_grasping_tasks = csv_data_frame.loc[
-                        (csv_data_frame['grasp'] == grasping_type) &
-                        (csv_data_frame['arm'] == arm) &
-                        (csv_data_frame['result'] == faces)]
-                    grasping_type_based_grasping_tasks[['t_x', 't_y', 't_z', 'success']].to_csv(
-                        join(result_dir_path, '{},{},{},{}.csv'.format(object_type, grasping_type,faces, arm)), index=False)
     else:
         print 'A directory we the stored information is required'
---
+++ grasping_learning_data_generator/cram2wordnet.py
@@ -0,0 +1,8 @@
+_cram_to_word_net_object_ = {'BOWL':'bowl.n.01', 'CUP': 'cup.n.01', 'SPOON' : 'spoon.n.01'}
+
+
+def get_word_net_object(cram_object, default_object=None):
+    if default_object:
+        return _cram_to_word_net_object_.get(cram_object, default_object)
+
+    return _cram_to_word_net_object_.get(cram_object, cram_object)
