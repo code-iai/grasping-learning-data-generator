60026c9c885a18b10193d48c8d6d10f509c4535a
Sebastian Koralewski <seba@cs.uni-bremen.de>
2019-05-29 13:18:59 +0200

    feature(cram2wordnet.py): Adds new objects

--- grasping_learning_data_generator/cram2wordnet.py
+++ grasping_learning_data_generator/cram2wordnet.py
@@ -1,4 +1,10 @@
-_cram_to_word_net_object_ = {'BOWL':'bowl.n.01', 'CUP': 'cup.n.01', 'SPOON' : 'spoon.n.01'}
+_cram_to_word_net_object_ = {'BOWL': 'bowl.n.01',
+                             'CUP': 'cup.n.01',
+                             'SPOON': 'spoon.n.01',
+                             'BREAKFAST-CEREAL': 'cereal.n.03',
+                             'DRAWER': 'drawer.n.01',
+                             'MILK': 'box.n.01'
+                             }
 
 
 def get_word_net_object(cram_object, default_object=None):
